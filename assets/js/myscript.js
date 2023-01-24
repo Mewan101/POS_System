cda3a9e7cde8ef87958f1b42448715f0294ac73c
Mewan101 <hamsampath@gmail.com>
2023-01-24 10:53:33 +0530

    Customer aded to the table

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -17,7 +17,21 @@ $("#custombtn").click(function () {
 });
 
 $("#itembtn").click(function () {
-    $("#page1").css("display" , "none");
+    $("#page1").css("display" , "none ");
     $("#page2").css("display" , "block");
     $("#page3").css("display" , "none");
+});
+
+
+$("#btnUpdate").click(function () {
+
+    var customerID = $("#basic-url-2").val();
+    var customerName = $("#basic-url-3").val();
+    var customerAddress = $("#basic-url-4").val();
+    var customerTP = $("#basic-url-5").val();
+    
+    // $("#idTable>tbody").append("<tr> <th>"+customerID+"</th> <td>"+customerName+"</td> <td>"+customerAddress+"</td> <td>"+customerTP+"</td> </tr>");
+
+    var row = `<tr> <th>${customerID}</th> <td>${customerName}</td> <td>${customerAddress}</td> <td>${customerTP}</td> </tr>`;
+    $("#idTable>tbody").append(row);
 });
